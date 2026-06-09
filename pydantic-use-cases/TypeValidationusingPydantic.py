a6962d41a6a1b8de56c740438f0f10e214d74e67
Mohd Uzaif <mohduzaif192@gmail.com>
2026-06-09 14:55:07 +0530

    add some more new concepts about type validation

--- pydantic-use-cases/TypeValidationusingPydantic.py
+++ pydantic-use-cases/TypeValidationusingPydantic.py
@@ -1,9 +1,18 @@
 from pydantic import BaseModel
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
+
+    # here we mentioned all the fields are required.
     name : str
     age : int
     weight : float
+    married : bool = False
+    contact_details : Dict[str, str]
+
+    # this is the field that we make it optional but we need to set a default value in it. 
+    allergies : Optional[List[str]] = None
+    
 
 
 # here we clearly see that we dont need to write similar code again again in the different function.
@@ -13,6 +22,9 @@ def insert_data_into_DB(patient : Patient):
     print(patient.name)
     print(patient.age)
     print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
 
     print('Data has been Inserted Successfully!')
 
@@ -27,7 +39,12 @@ def update_data_into_DB(patient : Patient):
 dictionary = {
     'name' : 'uzaif', 
     'age' : 25, 
-    'weight' : 62.4
+    'weight' : 62.4, 
+    # 'allergies' : ['dast', 'pllen'], 
+    'contact_details' : {
+        'email' : 'abc@gmail.com', 
+        'phone_no' : '1234567898'
+    }
 } 
 
 # here we are unpack the dictionary first before passing it.
@@ -37,5 +54,5 @@ dictionary = {
 patient = Patient.model_validate(dictionary)
 
 insert_data_into_DB(patient)
-update_data_into_DB(patient)
+# update_data_into_DB(patient)
 
