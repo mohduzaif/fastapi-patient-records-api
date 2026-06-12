4e16c68cd421e74cfb23434c55fbe5ca5e92cef9
Mohd Uzaif <mohduzaif192@gmail.com>
2026-06-12 17:02:23 +0530

    add new end-points for creating new patient

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
 from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
 import json
 
+
+# create a class for pydantic model that actually validate the data comes from the client side.
+class Patient(BaseModel):
+    
+    patient_id : Annotated[str, Field(..., description = 'Patient Id for each patient', example = 'P001')]
+    name : Annotated[str, Field(..., min_length = 3, max_length = 50, description = 'Name of the patient and lenght should be greater than 3 and less than 50', example = 'MS Dhoni')]
+    city : Annotated[str, Field(..., max_length = 50, description = 'Name of city where the patient is living')]
+    age : Annotated[int, Field(..., gt = 0, lt = 120, description = 'Age of the Patient')]
+    gender : Literal['Male', 'Female', 'Other']
+    height : Annotated[float, Field(..., gt = 0, description = 'Height of the patient in meters')] 
+    weight : Annotated[float, Field(..., gt = 0, description = 'Weight of the patient')]
+
+    # calculate bmi on the go when the validate of the data is done using pydantic model.
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        calculate_bmi = round(self.weight / self.height ** 2, 2)
+        return calculate_bmi
+    
+    # this field is calculated the verdict on the go at the time of data validation of pydantic model.
+    @computed_field
+    @property
+    def verdict(self) -> str:
+
+        if self.bmi < 18.5:
+            return 'Underweight'
+        if 18.5 < self.bmi < 24.9:
+            return 'Normal'
+        if 24.9 < self.bmi < 29.9:
+            return 'Overweight'
+        else:
+            return 'Obese'
+
+
+
+# create the FastAPI object to create the routes.
 app = FastAPI()
+
 
 # open and load data from JSON file. ------> Utility Function.
 def load_data():
@@ -10,6 +50,11 @@ def load_data():
 
     return data
 
+
+# open and save data into a JSON file. ----------> Utility Function.
+def save_data(data):
+    with open('patients.json', 'w', encoding = 'utf-8') as f:
+        json.dump(data, f, indent = 4, ensure_ascii = False)
 
 # home route.
 @app.get('/')
@@ -74,3 +119,37 @@ def sort_patients(sort_by : str = Query(..., description = 'Sort the Data on the
     )
     
     return sorted_patients
+
+
+# this route is responsible for the creation of new patient into the database/JSON file.
+@app.post('/create')
+def insert_patient(patient : Patient):
+
+    # load the existing data from the database/JSON file.
+    data = load_data()
+
+    # check whether the given patient is already exist or not.
+    if patient.patient_id in data:
+        raise HTTPException(
+            status_code = 400, 
+            detail = f'Patient with Patient_id {patient.patient_id} is already exist in a Database.'
+        )
+
+    # insert the data into a database/JSON file.
+    # extract the dictionary from the pydantic model using model_dump() by excluding patient_id due to our database structure.
+    temp_data = patient.model_dump(exclude = 'patient_id')
+
+    # stored this new patient into a actual data.
+    data[patient.patient_id] = temp_data
+
+    # save the data into a JSON file.
+    save_data(data)
+
+    # it will shows a proper status code and message to the developer who used our API.
+    return JSONResponse(
+        status_code = 201, 
+        content = {
+            'success' : True, 
+            'message' : 'Patient Created Successfully'
+        }
+    )
